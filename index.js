829679a32e2580dd38ad10cd05455cc7eaa39b88
roukell <62136980+roukell@users.noreply.github.com>
2020-07-20 22:21:47 +1000

    update express get function

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-const http = require("http"); 
-const fs = require("fs");
-const homePage = fs.readFileSync("index.html");
-const aboutPage = fs.readFileSync("about.html");
-const contactPage = fs.readFileSync("contact.html");
-const notFoundPage = fs.readFileSync("notfound.html");
+const express = require("express");
+const app = express();
+const path = require("path");
 
-const server = http.createServer((req, res) => {
-    if (req.url === "/about")
-        res.end(aboutPage)
-    else if (req.url === "/contact")
-        res.end(contactPage)
-    else if (req.url === "/")
-        res.end(homePage)
-    else {
-        res.writeHead(404)
-        res.end(notFoundPage)
-    }
+
+app.listen(3000, () => {
+    console.log("App listening on port 3000")
 })
 
-server.listen(3000);
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve("index.html"))
+})
+
+app.get("/about", (req, res) => {
+    res.sendFile(path.resolve("about.html"))
+})
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.resolve("contact.html"))
+})
